58a375f05db885d6315de13ae8e6930d8760b4bc
wuhao <1034400621@qq.com>
2022-12-07 15:01:17 +0800

    优化

--- WoAutoCollectionPlugin/Bot/FishBot.cs
+++ WoAutoCollectionPlugin/Bot/FishBot.cs
@@ -165,7 +165,7 @@ namespace WoAutoCollectionPlugin.Bot
                         Thread.Sleep(1000);
                         tt++;
 
-                        if (closed)
+                        if (closed || territoryType != DalamudApi.ClientState.TerritoryType)
                         {
                             PluginLog.Log($"task stopping");
                             return true;
@@ -249,10 +249,10 @@ namespace WoAutoCollectionPlugin.Bot
                         Thread.Sleep(1000);
                         tt++;
 
-                        if (closed)
+                        if (closed || territoryType != DalamudApi.ClientState.TerritoryType)
                         {
                             PluginLog.Log($"task stopping");
-                            return true;
+                            return false;
                         }
                     }
                     position = WoAutoCollectionPlugin.GameData.KeyOperates.MoveToPoint(position, Ground[CurrentPoint], territoryType, false, false);
@@ -365,7 +365,7 @@ namespace WoAutoCollectionPlugin.Bot
                 uint maxGp = player.MaxGp;
                 if (CurrentGround == 0) {
                     // C 狂风云海 灵飘尘  <14 双提 14-24 三提 >24单提
-                    if (yfishsw.ElapsedMilliseconds / 1000 > 8 && yfishsw.ElapsedMilliseconds / 1000 < 14)
+                    if (yfishsw.ElapsedMilliseconds / 1000 > 10 && yfishsw.ElapsedMilliseconds / 1000 < 14)
                     {
                         if (maxGp >= 400)
                         {
@@ -382,7 +382,7 @@ namespace WoAutoCollectionPlugin.Bot
                 } else if (CurrentGround == 2)
                 {
                     // B 旋风云海 灵罡风  <12 双提 >12 单提
-                    if (yfishsw.ElapsedMilliseconds / 1000 > 8 && yfishsw.ElapsedMilliseconds / 1000 < 12)
+                    if (yfishsw.ElapsedMilliseconds / 1000 > 10 && yfishsw.ElapsedMilliseconds / 1000 < 12)
                     {
                         if (maxGp >= 400)
                         {
@@ -393,7 +393,7 @@ namespace WoAutoCollectionPlugin.Bot
                 else if (CurrentGround == 1)
                 {
                     // A 摇风云海 灵飞电  <16 双提 >16单提
-                    if (yfishsw.ElapsedMilliseconds / 1000 > 8 && yfishsw.ElapsedMilliseconds / 1000 < 16)
+                    if (yfishsw.ElapsedMilliseconds / 1000 > 10 && yfishsw.ElapsedMilliseconds / 1000 < 16)
                     {
                         if (maxGp >= 400)
                         {
